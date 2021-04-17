9e5ce777a1a42f4bf35e9236aa9bb105d6c30898
hakkyson <hakku230@gmail.com>
2021-04-17 17:19:17 +0300

    Modify player speed

--- src/index.js
+++ src/index.js
@@ -172,22 +172,30 @@ function update() {
 }
 
 function handlePlayerMovement(player) {
+    const angle = cursors.up.isDown && cursors.left.isDown
+              ||  cursors.up.isDown && cursors.right.isDown
+              ||  cursors.down.isDown && cursors.left.isDown
+              ||  cursors.down.isDown && cursors.right.isDown
+
+    const BASE_SPEED = 160
+    const SPEED = angle ? Math.sqrt( BASE_SPEED * BASE_SPEED / 2 ) : BASE_SPEED
+
     if (cursors.left.isDown)
     {
-        player.setVelocityX(-160);
+        player.setVelocityX(-SPEED);
     }
     else if (cursors.right.isDown)
     {
-        player.setVelocityX(160);
+        player.setVelocityX(SPEED);
     }
 
     if (cursors.up.isDown)
     {
-        player.setVelocityY(-160);
+        player.setVelocityY(-SPEED);
     }
     else if (cursors.down.isDown)
     {
-        player.setVelocityY(160);
+        player.setVelocityY(SPEED);
     }
 }
 
