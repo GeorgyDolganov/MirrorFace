799d4b44993577682259b195e07bbcd7273f4fc5
Denis <selean1000@gmail.com>
2021-04-19 20:31:23 +0300

    Added raycaster enemy with damaging ray, ray reflects now configurable

---
+++ src/GameObjects/Enemies/RaycasterEnemy.js
@@ -0,0 +1,41 @@
+import Phaser from "phaser";
+import ReflectableRay from "../ReflectableRay";
+
+export default class RaycasterEnemy extends Phaser.Physics.Arcade.Sprite {
+
+    _ray;
+
+    constructor(scene) {
+        super(scene, 0, 0, "pyramidHead");
+
+        this._ray = new ReflectableRay({
+            scene, fromPoint: {x: 0, y: 0}, angle: 0
+        })
+
+        scene.physics.add.existing(this)
+    }
+
+    update(player) {
+        this.moveTo(player);
+        this._ray.setOrigin(this.calculateRayOrigin());
+        this._ray.setAngle(this.rotation);
+        this._ray.update();
+    }
+
+    calculateRayOrigin() {
+        let r = 30;
+        return {
+            x: this.x + r * Math.cos(this.rotation),
+            y: this.y + r * Math.sin(this.rotation)
+        }
+    }
+
+    moveTo(gameObject) {
+        this.rotation = Phaser.Math.Angle.Between(this.x, this.y, gameObject.x, gameObject.y);
+        if (Phaser.Math.Distance.Between(this.x, this.y, gameObject.x, gameObject.y) > 150) {
+            this.scene.physics.moveToObject(this, gameObject, 1000, 3 * 1000);
+        } else {
+            this.body.stop();
+        }
+    }
+}
--- src/GameObjects/Enemy.js
+++ src/GameObjects/Enemy.js
@@ -26,7 +26,6 @@ export default class Enemy extends Phaser.Physics.Arcade.Sprite {
 
     update(player, scene) {
         this.rotation = Phaser.Math.Angle.Between(this.x, this.y, player.x, player.y);
-        console.log(Phaser.Math.Distance.Between(this.x, this.y, player.x, player.y))
         if (Phaser.Math.Distance.Between(this.x, this.y, player.x, player.y) > 150) {
             scene.physics.moveToObject(this, player, 1000, 3 * 1000);
         } else {
