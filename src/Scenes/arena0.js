66a1175fa442e3c46cd95babdada80941474673e
Denis <selean1000@gmail.com>
2021-04-21 00:58:18 +0300

    Cleared trash

--- src/Scenes/arena0.js
+++ src/Scenes/arena0.js
@@ -30,21 +30,12 @@ import Skeleton from '../GameObjects/Enemies/Skeleton';
 import RoundManager from "../Managers/RoundManager";
 
 var raycaster;
-var ray;
-var graphics;
-var bullets;
 var obstacles
 var cursors;
-
-var staticObstacles;
-var reflectableRay;
-
-let tick = true;
 
 var stats = new Stats();
 stats.showPanel(1); // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild(stats.dom);
-
 
 Phaser.Geom.Line.fromAngle = function (x, y, angle, distance) {
     return new Phaser.Geom.Line(x, y, x + distance * Math.cos(angle), y + distance * Math.sin(angle));
@@ -90,7 +81,6 @@ export default class Arena0 extends Scene {
 
         //Анимации
         let skeletonAnims = this.anims.createFromAseprite('skeleton_legs');
-        console.log(skeletonAnims)
 
         //Создаем арену
         this.arena = this.add.group()
@@ -135,48 +125,14 @@ export default class Arena0 extends Scene {
         this.gameUI = new GameUI(this);
         this.add.existing(this.gameUI);
 
-
-        // var gr = this.add.graphics({
-        //     lineStyle: {
-        //         width: 1,
-        //         color: 0x00ff00
-        //     },
-        //     fillStyle: {
-        //         color: 0xff00ff
-        //     }
-        // });
-        // gr.fillRect(0,0, 300, 300);
-        //create ray
-        ray = raycaster.createRay({
-            origin: {
-                x: 400,
-                y: 300
-            }
-        });
-
-        //enable auto slicing field of view
-        ray.autoSlice = true;
-        //enable arcade physics body
-        ray.enablePhysics();
-        //set collision (field of view) range
-        ray.setCollisionRange(6000);
-
-        //cast ray
-        let intersection = ray.cast();
-
-        bullets = this.physics.add.group({
-            classType: Bullet,
-            maxSize: 30,
-            runChildUpdate: true
-        });
-
+        //Debug info
         window.scene = this;
+
         //create obstacles
         obstacles = this.add.group();
-        createObstacles(this, obstacles, bullets);
-        console.log(obstacles)
+        createObstacles(this, obstacles);
+
         //map obstacles
-        console.log("obstacles", obstacles.getChildren());
         raycaster.mapGameObjects(obstacles.getChildren(), true);
         raycaster.mapGameObjects(this.arena.walls.getChildren(), true);
 
@@ -184,46 +140,9 @@ export default class Arena0 extends Scene {
         this.RoundManager = new RoundManager(this, this.EnemiesManager);
         this.RoundManager.setRound(1);
 
-        staticObstacles = this.physics.add.staticGroup({
-            key: 'test',
-            frameQuantity: 5
-        });
 
-        this.player.setCollideWorldBounds(true)
-
-        this.physics.add.collider(this.player, staticObstacles);
-
+        this.player.setCollideWorldBounds(true);
         this.physics.add.collider(this.player, this.arena.walls);
-
-        this.physics.add.collider(this.player, bullets, (player, bullet) => {
-            player.onHit(bullet);
-            bullet.remove();
-        });
-        this.physics.add.collider(this.mirror, bullets, (mirror, bullet) => {
-            console.log("Mirror hit", bullet);
-
-            let mirrorLine = Phaser.Geom.Line.fromAngle(mirror.x, mirror.y, mirror.rotation, 200);
-            let bulletLine = Phaser.Geom.Line.fromAngle(bullet.x, bullet.y, bullet.rotation, 200);
-
-            let reflectionAngle = Phaser.Geom.Line.ReflectAngle(mirrorLine, bulletLine);
-            console.log(reflectionAngle);
-
-            this.physics.velocityFromRotation(mirror.rotation - (90 * Math.PI / 180), bullet.speed, bullet.body.velocity);
-        });
-
-        //draw ray
-        graphics = this.add.graphics({
-            lineStyle: {
-                width: 1,
-                color: 0x00ff00
-            },
-            fillStyle: {
-                color: 0xff00ff
-            }
-        });
-        let line = new Phaser.Geom.Line(ray.origin.x, ray.origin.y, intersection.x, intersection.y);
-        graphics.fillPoint(ray.origin.x, ray.origin.y, 3)
-        graphics.strokeLineShape(line);
 
 
         //Set camera to follow the player
@@ -239,76 +158,17 @@ export default class Arena0 extends Scene {
             'left': Phaser.Input.Keyboard.KeyCodes.A,
             'right': Phaser.Input.Keyboard.KeyCodes.D,
         });
-
-        ray.setAngle(-Math.PI / 2);
-
-        reflectableRay = new ReflectableRay({
-            scene: this,
-            fromPoint: {
-                x: 400,
-                y: 400
-            },
-            angle: 0.48
-        });
     }
 
     update(time, delta) {
         stats.begin();
-        reflectableRay.update();
 
         this.EnemiesManager.update(time, delta);
         this.RoundManager.update(time, delta);
-        //rotate ray
-        ray.setAngle(ray.angle + 0.005);
-        //cast ray
-        let intersection = ray.cast();
-
-        // let visibleObjects = ray.overlap();
-        // if (visibleObjects.length > 0) console.log(visibleObjects);
-        if (intersection.object === undefined) tick = true
-
-        if (tick) {
-            if (intersection.object !== undefined) {
-                console.log("hit", {
-                    object: intersection.object,
-                    segment: intersection.segment,
-                    pos: {
-                        x: intersection.x,
-                        y: intersection.y
-                    }
-                });
-                tick = false
-            }
-        }
 
         handlePlayerMovement(this.player, cursors);
         updateMirrorPosition(this);
 
-
-        //draw ray
-        graphics.clear();
-        let line = new Phaser.Geom.Line(ray.origin.x, ray.origin.y, intersection.x, intersection.y);
-        graphics.fillPoint(ray.origin.x, ray.origin.y, 3)
-        graphics.strokeLineShape(line);
-
-        if (intersection.object !== undefined && intersection.object.type == "Arc") {
-            let angle = Phaser.Math.Angle.Between(intersection.object.x, intersection.object.y, intersection.x, intersection.y);
-
-            let tangentAngle = Phaser.Math.Angle.Between(intersection.object.x, intersection.object.y,
-                intersection.x + intersection.object.width / 2 * Math.cos(angle),
-                intersection.y + intersection.object.width / 2 * Math.sin(angle));
-            let tangent = Phaser.Geom.Line.fromAngle(intersection.x, intersection.y, tangentAngle + Math.PI / 2, 100);
-
-            intersection.segment = tangent
-        }
-        if (intersection.segment !== undefined) {
-            let reflectionAngle = Phaser.Geom.Line.ReflectAngle(line, intersection.segment);
-
-            // draw reflected line
-            const REFLECTED_LINE_LENGTH = 500
-            let line2 = Phaser.Geom.Line.fromAngle(intersection.x, intersection.y, reflectionAngle, REFLECTED_LINE_LENGTH);
-            graphics.strokeLineShape(line2);
-        }
         stats.end();
     }
 }
--- src/functions/createObstacles.js
+++ src/functions/createObstacles.js
@@ -1,25 +1,8 @@
 import Player from "../GameObjects/Player";
 
-export default function createObstacles(scene, obstacles, bullets) {
-    //create rectangle obstacle
-    let obstacle = scene.add.rectangle(100, 100, 75, 75)
-        .setStrokeStyle(1, 0xff0000);
-    obstacles.add(obstacle, true);
-
-    //create line obstacle
-    obstacle = scene.add.line(400, 100, 0, 0, 200, 50)
-        .setStrokeStyle(1, 0xff0000);
-    obstacles.add(obstacle);
-
-    //create circle obstacle
-    obstacle = scene.add.circle(650, 100, 50)
-        .setStrokeStyle(1, 0xff0000);
-    obstacles.add(obstacle);
-
-    //create polygon obstacle
-    obstacle = scene.add.polygon(650, 500, [0, 0, 50, 50, 100, 0, 100, 75, 50, 100, 0, 50])
-        .setStrokeStyle(1, 0xff0000);
-    obstacles.add(obstacle);
+//TODO: remove all this
+export default function createObstacles(scene, obstacles) {
+    let obstacle;
 
     // Create reflection particles for grenade
     scene.reflectionParticles = []
@@ -31,7 +14,7 @@ export default function createObstacles(scene, obstacles, bullets) {
         scene.reflectionParticles.push(obstacle);
     }
 
-    //create image obstacle
+
     scene.player = new Player(scene, scene.healthBar);
 
     window.addEventListener('keypress', e => {
