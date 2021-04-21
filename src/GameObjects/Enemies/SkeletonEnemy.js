e2f7b46375d868e35ee13c26ef4d9b1d627bdd9d
Denis <selean1000@gmail.com>
2021-04-21 18:33:33 +0300

    Changed skeleton size

--- src/GameObjects/Enemies/SkeletonEnemy.js
+++ src/GameObjects/Enemies/SkeletonEnemy.js
@@ -8,12 +8,14 @@ export default class SkeletonEnemy extends RaycasterEnemy {
     constructor(scene, x, y, children) {
         super(scene, x, y, "skeleton")
         this.play({key: 'walk', repeat: -1})
+        this.setScale(0.7);
     }
 
     onUpdate(time, delta) {
         super.onUpdate(time, delta);
         this.rotation = Phaser.Math.Angle.Between(this.x, this.y, scene.player.x, scene.player.y) - 1.5;
-        this.healthBar.setPosition(this.x - 25, this.y + 60);
+        this.healthBar.setPosition(this.x - 25, this.y + 40);
+
     }
 
     _calculateRayOrigin() {
